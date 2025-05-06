59ae55f8eb2b499489ea02483487bb103cdb78de
Anghelo05112005 <76397432@idexpasco.edu.pe>
2025-05-06 14:54:02 -0500

    commit de js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("boton1").addEventListener("click", function() {
+    alert("¡Haz presionado el Botón 1!");
+  });
+  
+  document.getElementById("boton2").addEventListener("click", function() {
+    alert("¡Haz presionado el Botón 2!");
+  });
+  
